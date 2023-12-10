e29247a3ccb530d9b0b909d17215e7f8c86224f9
anjana732 <anjanasingh1257@gmail.com>
2023-12-11 00:26:11 +0530

    updated Student profile

--- public/javascripts/StudentProfile.js
+++ public/javascripts/StudentProfile.js
@@ -1,107 +1,3 @@
-/*function enableEdit() {
-  var inputField = document.getElementById("editableInput");
-  var editBtn = document.getElementById("editBtn");
-  var saveBtn = document.getElementById("saveBtn");
-  var cancelBtn = document.getElementById("cancelBtn");
-
-  inputField.removeAttribute("disabled");
-  editBtn.style.display = "none";
-  saveBtn.style.display = "inline-block";
-  cancelBtn.style.display = "inline-block";
-}
-
-function saveChanges() {
-  var inputField = document.getElementById("editableInput");
-  var editBtn = document.getElementById("editBtn");
-  var saveBtn = document.getElementById("saveBtn");
-  var cancelBtn = document.getElementById("cancelBtn");
-
-  inputField.setAttribute("disabled", "true");
-  editBtn.style.display = "inline-block";
-  saveBtn.style.display = "none";
-  cancelBtn.style.display = "none";
-}
-
-function cancelEdit() {
-  var inputField = document.getElementById("editableInput");
-  var editBtn = document.getElementById("editBtn");
-  var saveBtn = document.getElementById("saveBtn");
-  var cancelBtn = document.getElementById("cancelBtn");
-
-  inputField.setAttribute("disabled", "true");
-  editBtn.style.display = "inline-block";
-  saveBtn.style.display = "none";
-  cancelBtn.style.display = "none";
-}
-function enableGenderEdit() {
-  var inputField = document.getElementById("inputGender");
-  var editGenderBtn = document.getElementById("editGenderBtn");
-  var saveGenderBtn = document.getElementById("saveGenderBtn");
-  var cancelGenderBtn = document.getElementById("cancelGenderBtn");
-
-  inputField.removeAttribute("disabled");
-  editGenderBtn.style.display = "none";
-  saveGenderBtn.style.display = "inline-block";
-  cancelGenderBtn.style.display = "inline-block";
-}
-function saveGenderChanges() {
-  var inputField = document.getElementById("inputGender");
-  var editGenderBtn = document.getElementById("editGenderBtn");
-  var saveGenderBtn = document.getElementById("saveGenderBtn");
-  var cancelGenderBtn = document.getElementById("cancelGenderBtn");
-
-  inputField.setAttribute("disabled", "true");
-  editGenderBtn.style.display = "inline-block";
-  saveGenderBtn.style.display = "none";
-  cancelGenderBtn.style.display = "none";
-}
-
-function cancelGenderEdit() {
-  var inputField = document.getElementById("inputGender");
-  var editGenderBtn = document.getElementById("editGenderBtn");
-  var saveGenderBtn = document.getElementById("saveGenderBtn");
-  var cancelGenderBtn = document.getElementById("cancelGenderBtn");
-
-  inputField.setAttribute("disabled", "true");
-  editGenderBtn.style.display = "inline-block";
-  saveGenderBtn.style.display = "none";
-  cancelGenderBtn.style.display = "none";
-}
-function enableLocationEdit() {
-  var inputField = document.getElementById("location");
-  var editLocation = document.getElementById("editLocationBtn");
-  var saveLocation = document.getElementById("saveLocationBtn");
-  var cancelLocation = document.getElementById("cancelLocationBtn");
-
-  inputField.removeAttribute("disabled");
-  editLocation.style.display = "none";
-  saveLocation.style.display = "inline-block";
-  cancelLocation.style.display = "inline-block";
-}
-
-function saveLocationChanges() {
-  var inputField = document.getElementById("location");
-  var editLocation = document.getElementById("editLocationBtn");
-  var saveLocation = document.getElementById("saveLocationBtn");
-  var cancelLocation = document.getElementById("cancelLocationBtn");
-
-  inputField.setAttribute("disabled", "true");
-  editLocation.style.display = "inline-block";
-  saveLocation.style.display = "none";
-  cancelLocation.style.display = "none";
-}
-
-function cancelLocationEdit() {
-  var inputField = document.getElementById("location");
-  var editLocation = document.getElementById("editLocationBtn");
-  var saveLocation = document.getElementById("saveLocationBtn");
-  var cancelLocation = document.getElementById("cancelLocationBtn");
-
-  inputField.setAttribute("disabled", "true");
-  editLocation.style.display = "inline-block";
-  saveLocation.style.display = "none";
-  cancelLocation.style.display = "none";
-}*/
 //Toggle function
 function toggleEditElements(inputId, editBtnId, saveBtnId, cancelBtnId) {
   var inputField = document.getElementById(inputId);
@@ -232,11 +128,11 @@ function enableWorkEdit() {
 }
 
 function saveWorkChanges() {
-  toggleEditElements("editabWork", "editWorkBtn", "saveWorkBtn", "cancelWorkBtn");
+  toggleEditElements("editableWork", "editWorkBtn", "saveWorkBtn", "cancelWorkBtn");
 }
 
 function cancelWorkEdit() {
-  toggleEditElements("editabWork", "editWorkBtn", "saveWorkBtn", "cancelWorkBtn");
+  toggleEditElements("editableWork", "editWorkBtn", "saveWorkBtn", "cancelWorkBtn");
 }
 
 //Function for Education
@@ -266,3 +162,32 @@ function saveSkillsChanges() {
 function cancelSkillsEdit() {
   toggleEditElements("editableSkills", "editSkillsBtn", "saveSkillsBtn", "cancelSkillsBtn");
 }
+
+//Functions for course
+
+function enableCourseEdit() {
+  toggleEditElements("editableCourse", "editCourseBtn", "saveCourseBtn", "cancelCourseBtn");
+}
+
+function saveCourseChanges() {
+  toggleEditElements("editableCourse", "editCourseBtn", "saveCourseBtn", "cancelCourseBtn");
+}
+
+function cancelCourseEdit() {
+  toggleEditElements("editableCourse", "editCourseBtn", "saveCourseBtn", "cancelCourseBtn");
+}
+
+//Function for CGPA
+
+function enableCGPAEdit() {
+  toggleEditElements("editableCGPA", "editCGPABtn", "saveCGPABtn", "cancelCGPABtn");
+}
+
+function saveCGPAChanges() {
+  toggleEditElements("editableCGPA", "editCGPABtn", "saveCGPABtn", "cancelCGPABtn");
+}
+
+function cancelCGPAEdit() {
+  toggleEditElements("editableCGPA", "editCGPABtn", "saveCGPABtn", "cancelCGPABtn");
+}
+
